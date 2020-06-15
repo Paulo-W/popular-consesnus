5a56df094f5d2c73a0b844290cce08e540489add
Paul Wood <paulw@labs.epiuse.com>
2020-06-15 16:52:07 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {APIService} from './API.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myAmplifyProject';
+  todos: Array<any>;
+
+  constructor(private apiService: APIService) {
+  }
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  async ngOnInit() {
+    this.apiService.ListTodos().then((evt) => {
+      this.todos = evt.items;
+    });
+
+    this.apiService.OnCreateTodoListener.subscribe((evt) => {
+      const data = (evt as any).value.data.onCreateTodo;
+      this.todos = [...this.todos, data];
+    });
+  }
+
+  createTodo() {
+    this.apiService.CreateTodo({
+      name: 'Angular',
+      description: 'testing'
+    });
+  }
+
 }
