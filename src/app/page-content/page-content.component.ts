f48572c6cff7e1402579390eefeac36e24f106d6
Paul Wood <paulw@labs.epiuse.com>
2020-06-18 11:48:20 +0200

    Adds card layout to page-content

--- src/app/page-content/page-content.component.ts
+++ src/app/page-content/page-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faPlusCircle} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-page-content',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageContentComponent implements OnInit {
 
-  constructor() { }
+  faPlusCircle = faPlusCircle;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
