2a64e2127e40392c931522c70b48df894a299e05
Paul Wood <paulw@labs.epiuse.com>
2020-07-07 22:44:11 +0200

    Adds feature, display debate standing on the front end

--- src/app/interfaces/Debate.ts
+++ src/app/interfaces/Debate.ts
@@ -2,6 +2,7 @@ import {User} from './User';
 import {DebateTags} from '../enums/Tags';
 import {Channel} from './Channel';
 import {Team} from './Team';
+import {MessageInfo} from './MessageInfo';
 
 export interface Debate {
   id: number;
@@ -17,6 +18,22 @@ export interface Debate {
 }
 
 export class DebateInfo {
+  id: number;
+  createdBy: string;
+  channel: string;
+  title: string;
+  description: string;
+  date: Date;
+  days: number;
+  team1Name: string;
+  team2Name: string;
+  isTeam1: boolean;
+  isTeam2: boolean;
+
+  team1Votes: number;
+  team2Votes: number;
+  team1Percentage: number;
+  team2Percentage: number;
 
   constructor(private debate: Debate, private currentUser: User) {
     this.id = debate.id;
@@ -30,17 +47,29 @@ export class DebateInfo {
     this.team2Name = debate.team2.name;
     this.isTeam1 = debate.team1.members.includes(currentUser);
     this.isTeam2 = debate.team2.members.includes(currentUser);
+
+    this.team1Votes = Math.max(0, this.sumLikes(debate.team1));
+    this.team2Votes = Math.max(0, this.sumLikes(debate.team2));
+
+    this.team1Percentage = Math.round(this.mapPercentage(this.team1Votes));
+    this.team2Percentage = Math.round(this.mapPercentage(this.team2Votes));
   }
 
-  id: number;
-  createdBy: string;
-  channel: string;
-  title: string;
-  description: string;
-  date: Date;
-  days: number;
-  team1Name: string;
-  team2Name: string;
-  isTeam1: boolean;
-  isTeam2: boolean;
+  private sumLikes(team: Team): number {
+    return team.messages?.reduce((sum, current) => sum + this.getMessageLikes(current), 0) || 0;
+  }
+
+  private getMessageLikes(message: MessageInfo): number {
+    return (message.likeUsers?.length || 0) - (message.dislikeUsers?.length || 0);
+  }
+
+  private mapPercentage(teamNumber: number): number {
+    const base = this.team1Votes + this.team2Votes;
+
+    if (base === 0) {
+      return 0;
+    }
+
+    return (teamNumber / base) * 100;
+  }
 }
