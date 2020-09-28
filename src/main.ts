2fcb1f49cf11eb50754e1536c67f1a837a501f99
Paul Wood <paulw@labs.epiuse.com>
2020-09-28 11:48:26 +0200

    config storage

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
 import PubSub from '@aws-amplify/pubsub';
 import API from '@aws-amplify/api';
 import awsconfig from './aws-exports';
 import Amplify from 'aws-amplify';
-import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
-import { Auth } from '@aws-amplify/auth';
+import {AmazonAIPredictionsProvider} from '@aws-amplify/predictions';
+import Auth from '@aws-amplify/auth';
+import Storage from '@aws-amplify/storage';
 
 API.configure(awsconfig);
 PubSub.configure(awsconfig);
 Auth.configure(awsconfig);
+Storage.configure(awsconfig);
 Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
 if (environment.production) {
